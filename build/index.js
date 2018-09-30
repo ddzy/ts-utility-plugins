7fbc8f1a2701556678e779523c2ed46f5a99d0d0
yang <1766083035@qq.com>
2018-09-30 18:55:48 +0800

    feat * complete the setAttr&_init method

--- build/index.js
+++ build/index.js
@@ -3,6 +3,7 @@
  * create_time: 18-9-30
  * author: yyg
  */
+;
 var Utils;
 (function (Utils) {
     /**
@@ -23,4 +24,60 @@ var Utils;
         };
     }
     Utils.getWinRange = getWinRange;
+    /**
+     * 设置元素属性
+     * @param ele 元素
+     * @param options 属性配置
+     */
+    function setAttr(ele, options) {
+        for (var key in options) {
+            if (options.hasOwnProperty(key)) {
+                ele.setAttribute(key, options.key);
+            }
+        }
+        return ele;
+    }
+    Utils.setAttr = setAttr;
+    // /**
+    //  * 设置元素样式
+    //  * @param el 元素
+    //  * @param options 属性配置
+    //  */
+    // export function setCss(
+    //   ele: HTMLElement,
+    //   options: any,
+    // ): HTMLElement {
+    //   for(const item in options) {
+    //     if(options.hasOwnProperty(item)) {
+    //     }
+    //   }
+    //   return ele;
+    // }
 })(Utils || (Utils = {}));
+;
+var Line = /** @class */ (function () {
+    function Line(options) {
+        if (options === void 0) { options = {}; }
+        // public static createNew(): HTMLCanvasElement {
+        //   const oCanvas = document.createElement('canvas');
+        //   const { winWidth, winHeight } = Utils.getWinRange();
+        //   document.body.appendChild(oCanvas);
+        //   Utils.setAttr(oCanvas, {
+        //     width: winWidth,
+        //     height: winHeight,
+        //   });
+        // }
+        this.el = null;
+        this.pen = null;
+        this._init(options);
+    }
+    /**
+     * 初始化星空线
+     * @param options 配置项
+     */
+    Line.prototype._init = function (options) {
+        this.el = options.el || Line.createNew();
+    };
+    return Line;
+}());
+;
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@
  * author: yyg
  */
 
+interface ILineProps {
+  el?: string;
+};
 
 
 namespace Utils {
@@ -31,7 +34,82 @@ namespace Utils {
     };
   }
 
-}
+
+  /**
+   * 设置元素属性
+   * @param ele 元素
+   * @param options 属性配置
+   */
+  export function setAttr(
+    ele: HTMLElement,
+    options: any,
+  ): HTMLElement {
+    for (const key in options) {
+      if (options.hasOwnProperty(key)) {
+        ele.setAttribute(key, options.key);
+      }
+    }
+
+    return ele;
+  }
+
+  
+  // /**
+  //  * 设置元素样式
+  //  * @param el 元素
+  //  * @param options 属性配置
+  //  */
+  // export function setCss(
+  //   ele: HTMLElement,
+  //   options: any,
+  // ): HTMLElement {
+  //   for(const item in options) {
+  //     if(options.hasOwnProperty(item)) {
+
+  //     }
+  //   }
+
+  //   return ele;
+  // }
+
+};
+
+
+class Line {
+
+  // public static createNew(): HTMLCanvasElement {
+  //   const oCanvas = document.createElement('canvas');
+  //   const { winWidth, winHeight } = Utils.getWinRange();
+
+  //   document.body.appendChild(oCanvas);
+  //   Utils.setAttr(oCanvas, {
+  //     width: winWidth,
+  //     height: winHeight,
+  //   });
+  // }
+
+  private el: HTMLCanvasElement | null = null;
+
+  private pen: any = null;
+
+  public constructor(
+    options: ILineProps = {},
+  ) {
+    this._init(options);
+  }
+
+  /**
+   * 初始化星空线
+   * @param options 配置项
+   */
+  public _init(
+    options: ILineProps,
+  ): void {
+    this.el = options.el || Line.createNew();
+  }
+  
+
+};
 
 
 
