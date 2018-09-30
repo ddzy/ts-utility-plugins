3b51dc24d28911673b3f9dac89dfdc99ae9ea838
yang <1766083035@qq.com>
2018-09-30 18:18:36 +0800

    feat * complete the getEle method

--- build/index.js
+++ build/index.js
@@ -1,3 +1,16 @@
 "use strict";
-var oDiv = document.getElementById('app');
-console.log(oDiv);
+/**
+ * create_time: 18-9-30
+ * author: yyg
+ */
+var Utils;
+(function (Utils) {
+    /**
+     * 获取元素
+     * @param id 元素id
+     */
+    function getEle(id) {
+        return document.getElementById(id) || null;
+    }
+    Utils.getEle = getEle;
+})(Utils || (Utils = {}));
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
-const oDiv: HTMLElement | null = document.getElementById('app');
+/**
+ * create_time: 18-9-30
+ * author: yyg
+ */
 
-console.log(oDiv);
+
+
+namespace Utils {
+
+  /**
+   * 获取元素
+   * @param id 元素id
+   */
+  export function getEle(
+    id: string,
+  ): HTMLElement | null {
+    return document.getElementById(id) || null;
+  }
+
+}
+
 
