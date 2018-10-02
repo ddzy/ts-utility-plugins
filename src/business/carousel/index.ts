ce5f9f5699882a2d634b0122c279d24ebc03a3bd
yang <1766083035@qq.com>
2018-10-02 22:46:39 +0800

    style * 修改标注提示

--- src/business/carousel/index.ts
+++ src/business/carousel/index.ts
@@ -1,8 +1,7 @@
 /**
- * name: business-carousel 业务轮播
- * author: zhaoy_duan
- * create_time: 18-10-2
- * last_modified_time: 18-10-2 
+ * @name: business-carousel 业务轮播
+ * @author: zhaoy_duan
+ * @version 1.0.0
  */
 
 
