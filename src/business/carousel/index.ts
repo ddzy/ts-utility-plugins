21f6a85cee221fd8cecf4be9c8ae4ab026058d27
yang <1766083035@qq.com>
2018-10-02 18:00:07 +0800

    feat * 完成config&render&props基本结构

--- src/business/carousel/index.ts
+++ src/business/carousel/index.ts
@@ -4,3 +4,24 @@
  * create_time: 18-10-2
  * last_modified_time: 18-10-2 
  */
+
+namespace Carousel {
+
+  export namespace IProps {
+    export interface IConfigProps {}
+    export interface IRenderProps {} 
+  }
+
+
+  export function config(): any {
+    
+    return Carousel;
+  }
+
+
+  export function render() {
+    
+    return Carousel;
+  }
+
+}
