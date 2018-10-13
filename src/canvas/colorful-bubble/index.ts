a59357fd115090753dc5808061542cb40c936067
yang <1766083035@qq.com>
2018-10-13 16:35:51 +0800

    feat * 定义基本配置

--- src/canvas/colorful-bubble/index.ts
+++ src/canvas/colorful-bubble/index.ts
@@ -1,1 +1,19 @@
-console.log('colorful-bubble');
+/**
+ * @name canvas-colorful-bubble 五彩气泡特效插件
+ * @author yyg
+ * @version 1.0.0
+ */
+
+/**
+ * cvsWidth number 画布宽
+ * cvsHeight number 画布高
+ * cvsBgColor string 画布背景
+ * bubbleNum number 气泡数量
+ * bubbleSpeed number 气泡移动速度
+ * allowMouse boolean 是否允许鼠标交互
+ * bubbleColorArr string[] 气泡颜色数组
+ */
+
+namespace ColorfulBubble {
+  
+}
