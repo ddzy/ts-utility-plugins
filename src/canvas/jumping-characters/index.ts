4e64c3197959d3240f60bbca19b99bc7d020ae1c
yang <1766083035@qq.com>
2018-10-17 19:26:30 +0800

    feat * 完成默认配置项设置&接口类型限定

--- src/canvas/jumping-characters/index.ts
+++ src/canvas/jumping-characters/index.ts
@@ -16,3 +16,44 @@
  * @param safeDistance 安全距离(移动多远消失)
  * @param initialOpacity 初始透明度
  */
+
+
+namespace JumpingCharacters {
+
+  const yyg_settings: IProps.IRenderProps = {
+    cvsWidth: 500,
+    cvsHeight: 500,
+    cvsBgColor: '#fff',
+    fonts: [
+      '富强', '民主', '文明', '和谐',
+      '自由', '平等', '公正', '法治',
+      '爱国', '敬业', '诚信', '友善',
+    ],
+    fontColor: '#d50',
+    fontSize: 16,
+    safeDistance: 20,
+    initialOpacity: 1,
+  }
+
+  
+  export function render(
+    _props: IProps.IRenderProps,
+  ) {
+    
+  }
+
+
+  namespace IProps {
+    export interface IRenderProps {
+      cvsWidth?: number,
+      cvsHeight?: number,
+      cvsBgColor?: string,
+      fonts?: string[] | string,
+      fontColor?: string,
+      fontSize?: number,
+      safeDistance?: number,
+      initialOpacity?: number,
+    }
+  }
+
+}
