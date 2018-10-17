5f6a0b401cf94dc7720c8d829c01f71545ca4bcd
yang <1766083035@qq.com>
2018-10-17 19:11:21 +0800

    feat * 新增自定义配置参数说明

--- src/canvas/jumping-characters/index.ts
+++ src/canvas/jumping-characters/index.ts
@@ -4,3 +4,15 @@
  * @author: yyg
  * @version 1.0.0
  */
+
+
+/**
+ * @param cvsWidth 画布宽
+ * @param cvsHeight 画布高
+ * @param cvsBgColor 画布背景
+ * @param fonts   string | string[] 字体数组
+ * @param fontColor 字体颜色
+ * @param fontSize 字体大小
+ * @param safeDistance 安全距离(移动多远消失)
+ * @param initialOpacity 初始透明度
+ */
