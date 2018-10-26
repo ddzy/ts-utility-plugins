5c50ff9a5b85bea1cabb678f3553a20339b6e3e7
yang <1766083035@qq.com>
2018-10-26 15:29:56 +0800

    feat * 完成插件接口类型定义

--- src/business/tabs/index.ts
+++ src/business/tabs/index.ts
@@ -11,7 +11,37 @@
  * @param defaultActiveKey 初始化选中面板的key
  * @param tabBarGap tabs之间的间隙
  * @param tabBarStyle tab bar的样式对象
+ * @param tabBarLineStyle tab bar下面的线条样式
  * @param onChange 切换面板的回调
  * @param onTabClick tab被点击的回调
  * @param animated 是否使用动画
  */
+
+
+namespace Tabs {
+
+  export interface IDataSource {
+    tabPaneTitle?: {
+      iconUrl?: string,
+      text?: string,
+    };
+    tabPaneContent?: string;
+  }
+
+  export interface ITabsProps {
+    dataSource: IDataSource[];
+    type?: 'line' | 'card';
+    defaultActiveKey?: 1;
+    tabBarGap?: 5;
+    tabBarStyle?: object;
+    tabBarLineStyle?: object;
+    animated?: boolean;
+    onChange?: (activeKey: number | string) => void;
+    onTabClick?: () => void;
+  }
+
+  export function render(_props: ITabsProps): void {
+    
+  }
+
+}
