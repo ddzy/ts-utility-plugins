3ef896d4ba9a5248ab85128f38542cf1f392b5b0
yang <1766083035@qq.com>
2018-11-03 15:48:46 +0800

    feat * 完成chunk_method

--- src/business/lodash/index.ts
+++ src/business/lodash/index.ts
@@ -3,3 +3,44 @@
  * @author: yyg
  * @version 1.0.0
  */
+
+
+namespace lodash {
+
+  export namespace _Utils {
+    export function _isArray(obj: any): boolean {
+      return Array.isArray(obj);
+    }
+  }
+
+  export namespace _Array {
+
+    /**
+     * 创建一个元素数组，将元素分成大小的长度。如果数组无法均匀分割，
+     * 则最终的块将是剩余的元素。
+     * @param arr 数组
+     * @param size 分成的块
+     * @returns 新的数组
+     */
+    export function chunk(arr: any[], size: number = 1): any[] {
+      if (!_Utils._isArray(arr)) {
+        return [];
+      } else if (arr.length === 0) {
+        return [];
+      }
+      const newArr: any[] = [];
+
+      for (let i = 0; i < arr.length; i++) {
+        if (i % size === 0) {
+          newArr.push(arr.slice(i, i + size));
+        }
+      }
+
+      return newArr;
+    }
+
+  }
+
+}
+
+console.log(lodash._Array.chunk([1, 2, 3, 4], 5));
