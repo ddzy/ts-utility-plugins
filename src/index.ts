aecaded0a17c092cc390ad1bead054bd2f8b60d6
yang <1766083035@qq.com>
2019-05-13 16:00:31 +0800

    :white_check_mark: test SortDraggable styles props

--- src/index.ts
+++ src/index.ts
@@ -5,63 +5,14 @@ import {
 
 new Draggable.Sort({
   container: '#app',
+  dragWrapperStyle: {
+    backgroundColor: '#1890ff',
+    opacity: '.5'
+  },
+  dragOriginStyle: {
+    backgroundColor: 'red',
+  },
+  dragOriginActiveStyle: {
+    backgroundColor: 'blue',
+  },
 })
-
-
-
-// ? TEST
-
-// const source = document.getElementById('drag-wrapper') as HTMLDivElement;
-// const target = document.getElementById('drop-wrapper') as HTMLDivElement;
-
-// document.body.style.cssText += `
-//   margin: 0;
-//   padding: 0;
-// `;
-
-// source.style.cssText += `
-//   width: 200px;
-//   height: 200px;
-//   border: 1px solid #1890ff;
-//   cursor: move;
-//   transition: all .3s ease;
-// `;
-// target.style.cssText += `
-//   width: 800px;
-//   height: 600px;
-//   margin-top: 20px;
-//   background-color: #ccc;
-//   transition: all .3s ease;
-// `;
-
-// source.addEventListener('dragstart', (e) => {
-//   const target = e.target as HTMLDivElement;
-
-//   const transfer = e.dataTransfer as DataTransfer;
-//   transfer.setData('source', target.getAttribute('id') as string);
-
-//   source.style.cssText += `
-//     opacity: 0;
-//   `;
-// });
-
-// source.addEventListener('dragend', (e) => {
-//   source.style.cssText += `
-//     opacity: 1;
-//   `;
-// });
-
-// target.addEventListener('dragover', (e) => {
-//   e.preventDefault();
-// });
-
-// target.addEventListener('drop', (e) => {
-//   e.preventDefault();
-//   const target = e.target as HTMLDivElement;
-//   const transfer = e.dataTransfer as DataTransfer;
-//   const id = transfer.getData('source');
-
-//   target.appendChild(
-//     document.getElementById(id) as Node,
-//   );
-// });
