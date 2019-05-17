d2730c6a4626a8b4682789e860221db76b94c535
yang <1766083035@qq.com>
2019-05-17 17:36:14 +0800

    :construction: add  new method called _querySelector to ddzy's utility/dom

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,3 @@
-import {
-  Draggable,
-} from './ddzy/business/draggable/index';
+import utilityDOM from './ddzy/utility/dom/index';
 
-
-new Draggable.Sort({
-  container: '#app',
-  animate: true,
-  dataSource: [
-    {
-      titleText: `
-        <img src="https://avatars1.githubusercontent.com/u/33921398?s=40&v=4" />
-      `,
-      contentText: `
-        <h3>oneOne One</h3>
-      `,
-    },
-    {
-      titleText: '②',
-      contentText: 'twotwotwo'
-    },
-    {
-      titleText: '③',
-      contentText: 'threethreethree'
-    },
-  ],
-})
+utilityDOM._querySelector('div');
