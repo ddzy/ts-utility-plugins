b5ddfa00b4ec69b7d2f7c0535cbc497783318c8a
yang <1766083035@qq.com>
2019-05-18 15:30:47 +0800

    :construction: complete basic layout and style

--- src/ddzy/business/upload/dragger-upload/DraggerUpload.ts
+++ src/ddzy/business/upload/dragger-upload/DraggerUpload.ts
@@ -75,7 +75,69 @@ export class DraggerUpload {
   }
 
   private handleCreateDOM(): string {
-    let html: string = '';
+    let html: string = `
+      <div id="ddzy-upload-wrapper">
+        <div class="ddzy-upload-main">
+          <!-- 拖拽容器 -->
+          <div class="ddzy-upload-drag-container">
+            <input id="ddzy-upload-drag-input" type="file" multiple="true" accept="image/jpg, image/jpeg, image/gif, image/png" style="display: none;" />
+
+            <label for="ddzy-upload-drag-input" class="ddzy-upload-drag-content">
+              <div class="ddzy-upload-drag-icon-box">
+                <svg class="icon" aria-hidden="true">
+                  <use xlink:href="#icon-upload"></use>
+                </svg>
+              </div>
+              <div class="ddzy-upload-drag-title-box">
+                <h3>Click or Drag to upload</h3>
+              </div>
+              <div class="ddzy-upload-drag-description-box">
+                <p>Support mutiple files but only image</p>
+              </div>
+            </label>
+          </div>
+          <!-- 文件列表 -->
+          <div class="ddzy-upload-show-container">
+            <div class="ddzy-upload-show-content">
+              <ul class="ddzy-upload-show-list">
+                <li class="ddzy-upload-show-item">
+                  <div class="ddzy-upload-show-action-box">
+                    <div class="ddzy-upload-show-action">
+                      <span class="ddzy-upload-show-action-loading">
+                        <svg class="icon" aria-hidden="true">
+                          <use xlink:href="#icon-Loading"></use>
+                        </svg>
+                      </span>
+                      <span class="ddzy-upload-show-action-name" title="图片名称">
+                        miaomiao.jpg
+                      </span>
+                      <span class="ddzy-upload-show-action-preview" title="预览">
+                        <svg class="icon" aria-hidden="true">
+                          <use xlink:href="#icon-eye"></use>
+                        </svg>
+                      </span>
+                      <span class="ddzy-upload-show-action-send" title="上传">
+                        <svg class="icon" aria-hidden="true">
+                          <use xlink:href="#icon-upload1"></use>
+                        </svg>
+                      </span>
+                    </div>
+                  </div>
+                  <div class="ddzy-upload-show-close-box">
+                    <div class="ddzy-upload-show-close" title="移除">
+                      <svg class="icon" aria-hidden="true">
+                        <use  xlink:href="#icon-et-wrong"></use>
+                      </svg>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+
     return html;
   }
 
@@ -94,7 +156,118 @@ export class DraggerUpload {
   }
 
   private handleCreateStyle(): string {
-    let css: string = '';
+    let css: string = `
+      body, ul, li, p, h3 {
+        margin: 0;
+        padding: 0;
+      }
+      ul {
+        list-style-type: none;
+      }
+      label {
+        display: block;
+        cursor: pointer;
+      }
+
+      #ddzy-upload-wrapper {
+        height: 100%;
+      }
+      .ddzy-upload-main {
+        height: 100%;
+        user-select: none;
+        cursor: pointer;
+      }
+
+
+      /* 拖拽上传部分 */
+      .ddzy-upload-drag-container {
+        border: 1px dashed #666;
+        background-color: #f7f7f7;
+        cursor: pointer;
+      }
+      .ddzy-upload-drag-container:hover {
+        border-color: #1890ff;
+      }
+      .ddzy-upload-drag-content {
+        padding: 8px;
+        text-align: center;
+      }
+      .ddzy-upload-drag-icon-box {
+        margin-top: 8px;
+      }
+      .ddzy-upload-drag-title-box {
+        margin-top: 22px;
+      }
+      .ddzy-upload-drag-description-box {
+        margin-top: 10px;
+        margin-bottom: 16px;
+      }
+      .ddzy-upload-drag-icon-box svg {
+        color: #1890ff;
+        min-width: 40px;
+        min-height: 40px;
+      }
+      .ddzy-upload-drag-title-box h3 {
+        color: #888;
+      }
+      .ddzy-upload-drag-description-box p {
+        color: #999;
+        font-size: 14px;
+      }
+
+
+      /* 文件列表部分 */
+      .ddzy-upload-show-container {
+        margin-top: 8px;
+      }
+      .ddzy-upload-show-content {
+
+      }
+      .ddzy-upload-show-list {
+
+      }
+      .ddzy-upload-show-item {
+        display: flex;
+        padding: 4px 8px;
+        border: 1px solid #ddd;
+        color: #666;
+        transition: all .3s ease;
+      }
+      .ddzy-upload-show-item:hover {
+        background-color: #f3f4f5;
+      }
+      .ddzy-upload-show-item svg:hover {
+        color: #1890ff;
+      }
+      .ddzy-upload-show-action-box {
+        flex: 8;
+      }
+      .ddzy-upload-show-action {
+
+      }
+      .ddzy-upload-show-action > span {
+        display: inline-block;
+      }
+      .ddzy-upload-show-action-preview {
+        margin-left: 8px;
+      }
+      .ddzy-upload-show-action-send {
+        margin-left: 4px;
+      }
+      .ddzy-upload-show-action-name {
+        margin-left: 16px;
+      }
+      .ddzy-upload-show-action-loading {
+
+      }
+      .ddzy-upload-show-close-box {
+        flex: 1;
+      }
+      .ddzy-upload-show-close {
+        text-align: right;
+      }
+    `;
+
     return css;
   }
 
