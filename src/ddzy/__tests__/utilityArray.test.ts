5f921622e69966e9312ffbc9b1fe79109b1471f8
yang <1766083035@qq.com>
2019-05-24 14:04:49 +0800

    :white_check_mark: test toFlatArrayOutPlace method

--- src/ddzy/__tests__/utilityArray.test.ts
+++ src/ddzy/__tests__/utilityArray.test.ts
@@ -36,6 +36,50 @@ describe('utilityArray tests', () => {
         expect(utilityArray.isStrictArray(v)).toBeFalsy();
       }
     });
-  })
+  });
+
+  describe('toFlatArrayOutPlace tests', () => {
+    test('toFlatArrayOutPlace should flatten the origin array', () => {
+      const received = [
+        'duan',
+        1998,
+        [
+          'a',
+          'b',
+          [
+            'c',
+            'd',
+            [
+              'e',
+              'f',
+              123
+            ],
+          ],
+        ],
+        {
+          name: 'duan',
+          age: 20,
+        },
+      ];
+      const expected = [
+        'duan',
+        1998,
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        123,
+        { name: 'duan', age: 20 },
+      ];
+
+      const result = utilityArray.toFlatArrayOutPlace(received);
+
+      for (const [outerI, outerV] of result.entries()) {
+        expect(outerV).toEqual(expected[outerI]);
+      }
+    });
+  });
 
 });
