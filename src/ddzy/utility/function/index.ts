b638f604e8f43be9e37572e1e0f22873a55b230f
yang <1766083035@qq.com>
2019-05-25 13:17:00 +0800

    :construction: add new methods named `_bind`, `_call`

--- src/ddzy/utility/function/index.ts
+++ src/ddzy/utility/function/index.ts
@@ -1,5 +1,14 @@
 export interface IUtilityFunctionProps {
   isFunction(el: any): boolean;
+
+  _call(context: any, ...args: any[]): void;
+  _bind(context: any): (args: any[]) => any;
+}
+
+// TODO: 提取至utilityOthers
+function isNull(origin: any) {
+  return typeof origin !== undefined
+    && origin == undefined;
 }
 
 const utilityFunction: IUtilityFunctionProps = {
@@ -11,6 +20,32 @@ const utilityFunction: IUtilityFunctionProps = {
   isFunction(ele) {
     return typeof ele === 'function';
   },
+
+  _call(context, ...args) {
+    // TODO: 使用`keyof typeof Function`来解决索引签名报错(`元素隐式具有 "any" 类型，因为类型“Function”没有索引签名`)的问题
+    const funcName = this['name' as keyof typeof utilityFunction] as any;
+
+    if ( isNull(context) ) {
+      let w = window as any;
+
+      w[funcName] = this;
+      w[funcName](...args);
+      delete w[funcName];
+    } else {
+      context[funcName] = this;
+      context[funcName](...args);
+      delete context[funcName];
+    }
+  },
+
+  _bind(context) {
+    const that = this;
+    Function.prototype['_call' as 'prototype'] = utilityFunction._call;
+
+    return function (args) {
+      that['_call'](context, ...args);
+    }
+  }
 
 };
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-import ES6Achieve from './ddzy/business/es6_achieve/index';
+import utilityFunction from './ddzy/utility/function/index';
 
-var result = ES6Achieve._const('key', {});
+// utilityFunction._call('push', {}, 123);
 
-result.key = {};
+Function.prototype['_call' as 'prototype'] = utilityFunction._call;
+
+
+const origin = {
+  name: 'duan',
+};
+
+const target = {
+  say(...args: any[]) {
+    console.log(this['name' as keyof typeof target]);
+    console.log(args);
+  },
+};
+
+
+// target.say['_call' as 'prototype'](origin, 1, 2, 3, 4);
+// target.say['_call' as 'prototype'](null, 1, 2, 3, 4);
+
+Function.prototype['_bind' as 'prototype'] = utilityFunction._bind;
+
+target.say['_bind' as 'prototype'](origin)(['one', 'two', 'three'])
