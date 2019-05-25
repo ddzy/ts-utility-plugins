ccb60f0afbb7545d6756baae659de80c9ae729f1
yang <1766083035@qq.com>
2019-05-25 14:13:40 +0800

    :white_check_mark: test `_bind`, `_call`, `isNull`, `isUndefined`

--- src/ddzy/__tests__/utilityOthers.test.ts
+++ src/ddzy/__tests__/utilityOthers.test.ts
@@ -103,4 +103,31 @@ describe('utilityOthers tests', () => {
       received.program.related.website[0].name = 'received';
     });
   });
+
+  describe('isNull', () => {
+    test('isNull should return `false` if received a non null value', () => {
+      const received = [
+        0,
+        '',
+        undefined,
+        {},
+        [],
+        function() {},
+      ];
+
+      for (const v of received) {
+        expect(utilityOthers.isNull(v)).toBeFalsy();
+      }
+    });
+
+    test('isNull should return `true` if received a null value', () => {
+      const received = [
+        null,
+      ];
+
+      for (const v of received) {
+        expect(utilityOthers.isNull(v)).toBeTruthy();
+      }
+    });
+  })
 });
