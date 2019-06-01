658fb7fd68491e56657c598ae9e143ea1ceee8eb
yang <1766083035@qq.com>
2019-06-01 09:48:52 +0800

    :white_check_mark: test `getParamNames` method

--- src/ddzy/__tests__/utilityFunction.test.ts
+++ src/ddzy/__tests__/utilityFunction.test.ts
@@ -74,4 +74,24 @@ describe('utilityFunction', () => {
     })
   })
 
+  describe('getParamNames', () => {
+    test("getParamsNames should receive lots of parameters and return the array of each parameter's name.", () => {
+      const received = [
+        function (..._args: any[]) { },
+        function func1(_name: string, _age: number) { },
+        function func2(_name: string, _age: number, _address: string, ..._args: any[]) {},
+      ];
+      const expected = [
+        [],
+        ['_name', '_age'],
+        ['_name', '_age', '_address'],
+      ];
+
+      for (const [i, v] of received.entries()) {
+        const current = utilityFunction.getParamNames(v);
+        expect(current).toEqual(expected[i]);
+      }
+    });
+  });
+
 });
