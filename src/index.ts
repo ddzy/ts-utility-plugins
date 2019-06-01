d11fbed2e8a5b8e6ecd233ec0b6737702e199e24
yang <1766083035@qq.com>
2019-06-01 11:42:48 +0800

    :sparkles: add new bucket named utilityString, and add new method named `getRandomStr`

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
-import utilityFunction from "./ddzy/utility/function";
+import utilityString from "./ddzy/utility/string";
 
-const result1 = utilityFunction.getParamNames(
-  function (...args: any[]) { },
+const result1 = utilityString.getRandomStr();
+const result2 = utilityString.getRandomStr([]);
+const result3 = utilityString.getRandomStr(
+  ['a', 'b', 'c', '1', '2', '3', '_', '-'],
+  8,
 );
-const result2 = utilityFunction.getParamNames(
-  function func(name,age) {},
+const result4 = utilityString.getRandomStr(
+  undefined,
+  undefined,
+  false,
 );
-const result3 = utilityFunction.getParamNames(
-  function func(name, age, address, ...args) {},
+const result5 = utilityString.getRandomStr(
+  undefined,
+  undefined,
+  true,
 );
 
-console.log(result1);
+
+console.log(result1)
 console.log(result2);
 console.log(result3);
+console.log(result4);
+console.log(result5);
