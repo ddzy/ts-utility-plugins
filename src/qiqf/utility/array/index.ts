8ca0b5c65f64017c3d63388ec382fd63557cd1f5
qiqf <723469354@qq.com>
2019-07-04 12:41:59 +0800

    featrue: add arrayChunk&compact, doc: add to README.md&CHANGELOG.md

--- src/qiqf/utility/array/index.ts
+++ src/qiqf/utility/array/index.ts
@@ -7,3 +7,22 @@ export function arrayMax(args: InterfaceUtil.IArraySize<number>):number {
 export function arrayMin(args: InterfaceUtil.IArraySize<number>): number {
     return Math.min.apply(null, args);
 }
+
+/**
+ * 将数组划分为指定大小的数组
+ * @param args
+ * @param size
+ */
+export function arrayChunk(args: InterfaceUtil.IArrayAny, size: number): InterfaceUtil.IArrayAny {
+    return Array.from({length: Math.ceil(args.length / size)}, (_v: any, index: number) => {
+        return args.slice(index * size, index * size + size);
+    })
+}
+
+/**
+ * 从数组中移除 [false, null, '', 0, undefined] 值
+ * @param args
+ */
+export function compact(args: InterfaceUtil.IArrayAny): InterfaceUtil.IArrayAny {
+    return args.filter(Boolean)
+}
--- src/qiqf/utility/test/test-array.ts
+++ src/qiqf/utility/test/test-array.ts
@@ -1,8 +1,12 @@
 import {
     arrayMax,
-    arrayMin
+    arrayMin,
+    arrayChunk,
+    compact
 } from '../array'
 const numberArrays = [1,2,3,4,5];
 
-// console.log(arrayMax(numberArrays));
-// console.log(arrayMin(numberArrays));  1
+// console.log(arrayMax(numberArrays));   // 5
+// console.log(arrayMin(numberArrays));   // 1
+// console.log(arrayChunk(numberArrays, 3));   // [[1,2,3], [4,5]]
+// console.log(compact(numberArrays));  // [1,2,3,4,5]
