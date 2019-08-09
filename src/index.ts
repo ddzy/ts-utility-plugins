bd41f6349cbccb6309bcf5995356ff5f67f539dd
yang <1766083035@qq.com>
2019-08-09 16:57:36 +0800

    :sparkles: add new method named `trunk` into ddzy/utility/array bucket

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,29 @@
-import utilityAlgorithm from "./ddzy/utility/algorithm";
+import utilityArray from "./ddzy/utility/array";
 
-const _findIndex = utilityAlgorithm.ES6Achieve._findIndex;
+const trunk = utilityArray.trunk;
 
 
 // ? 空数组
 const s1: number[] = [];
-const p1 = _findIndex<number>(s1, (v) => {
-  return v > 0;
-});
+const p1 = trunk<number>(s1);
 console.log(p1);
 
 console.log('------------------------');
 
-// ? 数字数组, 未找到
-const s2: number[] = [23, 4, 2, 545, -2];
-const p2 = _findIndex<number>(s2, (v) => {
-  return v > 600;
-});
+// ? 普通数字数组
+const s2: number[] = [1, 2, 3, 4, 5, 6, 7];
+const p2 = trunk<number>(s2);
 console.log(p2);
 
-console.log('--------------------------');
+console.log('-------------------------');
 
-// ? 数字数组, 找到
-const s3: number[] = [1, 2, 3, -2, -4, 0];
-const p3 = _findIndex<number>(s3, (v) => {
-  return v < 0;
-});
+// ? 任意分片长度
+const s3: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const p3 = trunk<number>(s3, 2);
+const p4 = trunk<number>(s3, 5);
+const p5 = trunk<number>(s3, 7);
+const p6 = trunk<number>(s3, 10);
 console.log(p3);
-
-console.log('---------------------------');
-
-// ? 数字数组, this上下文
-const context = {
-  secret: 'ddzy',
-};
-const s4: number[] = [1, 2, 3, 4, 5];
-const p4 = _findIndex<number, typeof context>(s4, function (v) {
-  console.log(this);
-
-  return v > 3;
-}, context);
 console.log(p4);
+console.log(p5);
+console.log(p6);
