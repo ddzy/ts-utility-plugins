ad84d6142a34ee0833e68d059d07abe8562c534f
yang <1766083035@qq.com>
2019-08-13 08:39:42 +0800

    :white_check_mark: test ddzy's method named `compose`

--- src/ddzy/__tests__/utilityFunction.test.ts
+++ src/ddzy/__tests__/utilityFunction.test.ts
@@ -157,4 +157,42 @@ describe('utilityFunction', () => {
     });
   });
 
+  describe('compose', () => {
+    test('compose should invoke function from right to left', () => {
+      function func1() {
+        return func2() * 2;
+      }
+      function func2() {
+        return func3() * 3;
+      }
+      function func3() {
+        return 4;
+      }
+
+      function func4() {
+        return func5() + '*' + 'yang';
+      }
+      function func5() {
+        return func6() + '*' + 'zhao';
+      }
+      function func6() {
+        return 'duan';
+      }
+
+      const received = {
+        s1: [func1, func2, func3],
+        s2: [func4, func5, func6],
+      };
+      const expected = {
+        p1: 24,
+        p2: 'duan*zhao*yang',
+      };
+
+      const r1 = utilityFunction.compose(...received.s1);
+      const r2 = utilityFunction.compose(...received.s2);
+
+      expect(r1).toBe(expected.p1);
+      expect(r2).toBe(expected.p2);
+    });
+  });
 });
