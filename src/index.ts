c267c65cf6492bb992f6f31716cf3fd87cead992
yang <1766083035@qq.com>
2019-09-02 08:43:57 +0800

    :sparkles: add new method named `drop` into ddzy/utility/array bucket

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import { trim } from "./ddzy/utility/string/trim";
+import { drop } from "./ddzy/utility/array/drop";
 
-// ? 空字符串
-const s1 = '';
-const p1 = trim(s1);
+// ? 空数组 + 默认值
+const s1: number[] = [];
+const p1 = drop(s1);
 console.log(p1);
 
-console.log('---------------');
+console.log('-----------------');
 
-// ? 字符串 + 首尾空格 + 默认字符
-const s2 = '  ddzy   ';
-const p2 = trim(s2);
+// ? 纯数字数组 + 自定义数值
+const s2: number[] = [1, 2, 3, 4, 5, 6];
+const p2 = drop<number>(s2, 3);
 console.log(p2);
 
-console.log('----------------');
+console.log('--------------------');
 
-// ? 字符串 + 指定字符
-const s3 = 'wow, i like program!';
-const s4 = 'wow, i love you, wow';
-const p3 = trim(s3, 'wow');
-const p4 = trim(s4, 'wow');
+// ? 不应该改变源数组
+const s3: number[] = [100, 300, 500];
+const p3 = drop<number>(s3, 0);
 console.log(p3);
-console.log(p4);
+p3.push(700);
+console.log(s3);
+console.log(p3);
