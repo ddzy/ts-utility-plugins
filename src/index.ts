c419cab2e0a2cb4ed5edcfed65da39f74a09d273
yang <1766083035@qq.com>
2019-09-09 10:28:01 +0800

    :sparkles: add new method into ddzy/utility/others bucket, names `eq`

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
-import { head } from "./ddzy/utility/array/head";
+import { eq } from "./ddzy/utility/others/eq";
 
-// ? 普通数字数组
-const s1: number[] = [1, 2, 3, 4, 5];
-const p1 = head<number>(s1);
+// ? 基本类型值
+const s1 = [100, 100];
+const p1 = eq(s1[0], s1[1]);
 console.log(p1);
 
-console.log('-----------------');
+console.log('----------------');
 
-// ? 空数组
-const s2: any[] = [];
-const p2 = head<any>(s2);
+// ? 引用类型值, 指向不同的内存地址
+const s2 = [{}, {}];
+const p2 = eq(s2[0], s2[1]);
 console.log(p2);
+
+console.log('------------------');
+
+// ? 引用类型值, 指向相同的内存地址
+const o3 = {};
+const s3 = [o3, o3];
+const p3 = eq(s3[0], s3[1]);
+console.log(p3);
+
+console.log('-------------------');
+
+// ? 都为NaN, 也是相等
+const s4 = [NaN, NaN];
+const p4 = eq(s4[0], s4[1]);
+console.log(p4);
