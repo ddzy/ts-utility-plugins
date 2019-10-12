ff657ab6c5afcae8a7acb728520fc32be2654a44
yang <1766083035@qq.com>
2019-10-12 15:01:17 +0800

    :sparkles: add new method named `inRange` into ddzy/utility/number bucket

---
+++ src/ddzy/utility/number/inRange/index.ts
@@ -0,0 +1,20 @@
+import { isUndefined } from "../../others/isUndefined";
+import { numberLiteralTypeAnnotation } from "@babel/types";
+
+
+/**
+ * 检查指定值是否在 start 与 end 之间, 但是不包括 end
+ * @param value 需要检验的值
+ * @param start 起始位置
+ * @param end 终止位置
+ */
+export function inRange(
+  value: number,
+  start: number,
+  end: number,
+) {
+  start = start > end ? end : start;
+  end = start > end ? start : end;
+
+  return value >= start && value < end;
+}
