c224c0724a4e559d10ce768d76efb2588066e193
ddzy <1766083035@qq.com>
2020-10-28 11:03:17 +0800

    :hammer: rewrite the method named `deepClone`

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,30 @@
-import create from "./ddzy/utility/object/create";
+import deepClone from "./ddzy/utility/others/deepClone";
 
-
-const s1 = {
-  name: 'duanzhaoyang',
-  age: 22,
-};
-const p1 = create(s1, {});
-console.log(p1);
-
-const s2 = {};
-const p2 = create(s2, {
-  skill: 'running',
-});
-console.log(p2);
-
-const s3 = null;
-const p3 = create(s3, {});
-console.log(p3);
-
-const s4 = null;
-const p4 = create(s4, {
+const origin = {
   a: 1,
-  b: 2,
-})
-console.log(p4);
+  b: [1, 2, 3, 4],
+  c: {
+    d: 1,
+    e: 2,
+    f: [
+      {
+        g: 1,
+        h: 2,
+      },
+      {
+        i: 1,
+        j: 2,
+      },
+    ],
+  },
+};
+
+const target = deepClone(origin);
+
+console.log(origin, target);
+console.log(origin === target);
+
+target.b.push(5);
+target.c.d = 2;
+
+console.log(origin, target);
